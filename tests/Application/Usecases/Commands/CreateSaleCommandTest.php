01c5fbe6b3a2f491c2890539142144786b55be70
Ryan Grippeling <R.Grippeling@hotmail.com>
2020-03-30 18:32:36 +0200

    Make sure that the repository saves the sale when the command is called

--- tests/Application/Usecases/Commands/CreateSaleCommandTest.php
+++ tests/Application/Usecases/Commands/CreateSaleCommandTest.php
@@ -3,24 +3,39 @@
 
 use CleanArchitecture\Application\Usecases\Commands\CreateSaleCommand;
 use CleanArchitecture\Application\Usecases\Repositories\SaleRepository;
+use CleanArchitecture\Domain\Models\Product;
+use CleanArchitecture\Domain\Models\Sale;
 use PHPUnit\Framework\TestCase;
 
 class CreateSaleCommandTest extends TestCase
 {
+    private CreateSaleCommand $command;
+    private string $customerId;
+
     /**
      * @var SaleRepository|\Prophecy\Prophecy\ObjectProphecy
      */
     private $saleRepository;
-    private CreateSaleCommand $command;
+    /**
+     * @var Product|\Prophecy\Prophecy\ObjectProphecy
+     */
+    private $product;
 
     public function setUp(): void
     {
         $this->saleRepository = $this->prophesize(SaleRepository::class);
         $this->command = new CreateSaleCommand($this->saleRepository->reveal());
+
+        $this->customerId = 'customerId';
+        $this->product = $this->prophesize(Product::class);
+        $this->product->amountInCents()->willReturn(599);
     }
 
-    public function test_is_instantiable()
+    public function test_command_will_save_on_happy_flow()
     {
-        $this->assertInstanceOf(CreateSaleCommand::class, $this->command);
+        $expectedSaleToBeSaved = new Sale($this->customerId, [$this->product->reveal()]);
+        $this->saleRepository->save($expectedSaleToBeSaved)->shouldBeCalledOnce();
+
+        $this->command->execute($this->customerId, [$this->product->reveal()]);
     }
 }
